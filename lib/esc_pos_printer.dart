8dfaaa5f50636333dee5a0c54f157389d8f39630
Clapton Orioste <clapton.five@gmail.com>
2024-05-21 16:14:47 +0800

    feat: update

--- lib/esc_pos_printer.dart
+++ lib/esc_pos_printer.dart
@@ -9,3 +9,4 @@ library esc_pos_printer;
 
 export 'src/enums.dart';
 export 'src/network_printer.dart';
+export 'src/usb_manager.dart';
---
+++ lib/src/usb_manager.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:escposprinter/escposprinter.dart';
+
+import './enums.dart';
+
+class PrinterUsbManager {
+  Future<List> getDevices() async {
+    List devices = await Escposprinter.getUSBDeviceList;
+    return devices;
+  }
+
+  Future<void> connectDevice(int vendor, int product) async {
+    await Escposprinter.connectPrinter(vendor, product);
+    return;
+  }
+
+  Future<PosPrintResult> writeBytes(List<int> bytes) async {
+    final Completer<PosPrintResult> completer = Completer();
+    await Escposprinter.printBytes(bytes);
+    completer.complete(PosPrintResult.success);
+    return completer.future;
+  }
+
+  Future<PosPrintResult> printTicket(List<int> bytes) {
+    if (bytes.isEmpty) {
+      return Future<PosPrintResult>.value(PosPrintResult.ticketEmpty);
+    }
+    return writeBytes(bytes);
+  }
+}
