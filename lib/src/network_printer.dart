17f84bfc59b2d95c686e3a4b480aec41348351f0
Clapton Orioste <clapton.five@gmail.com>
2024-06-19 16:14:30 +0800

    feat: update network printer

--- lib/src/network_printer.dart
+++ lib/src/network_printer.dart
@@ -7,182 +7,42 @@
  */
 
 import 'dart:io';
-import 'dart:typed_data' show Uint8List;
-import 'package:esc_pos_utils/esc_pos_utils.dart';
-import 'package:image/image.dart';
 import './enums.dart';
 
-/// Network Printer
-class NetworkPrinter {
-  NetworkPrinter(this._paperSize, this._profile, {int spaceBetweenRows = 5}) {
-    _generator =
-        Generator(paperSize, profile, spaceBetweenRows: spaceBetweenRows);
-  }
-
-  final PaperSize _paperSize;
-  final CapabilityProfile _profile;
+/// Printer Network Manager
+class PrinterNetworkManager {
   String? _host;
   int? _port;
-  late Generator _generator;
-  late Socket _socket;
+  Duration? _timeout;
 
-  int? get port => _port;
-  String? get host => _host;
-  PaperSize get paperSize => _paperSize;
-  CapabilityProfile get profile => _profile;
-
-  Future<PosPrintResult> connect(String host,
-      {int port = 91000, Duration timeout = const Duration(seconds: 5)}) async {
+  /// Select a network printer
+  ///
+  /// [timeout] is used to specify the maximum allowed time to wait
+  /// for a connection to be established.
+  void selectPrinter(
+    String host, {
+    int port = 9100,
+    Duration timeout = const Duration(seconds: 5),
+  }) {
     _host = host;
     _port = port;
-    try {
-      _socket = await Socket.connect(host, port, timeout: timeout);
-      _socket.add(_generator.reset());
+    _timeout = timeout;
+  }
+
+  Future<PosPrintResult> printTicket(ticket) {
+    if (_host == null || _port == null) {
+      return Future<PosPrintResult>.value(PosPrintResult.printerNotSelected);
+    } else if (ticket == null || ticket.bytes.isEmpty) {
+      return Future<PosPrintResult>.value(PosPrintResult.ticketEmpty);
+    }
+
+    return Socket.connect(_host, _port ?? 9100, timeout: _timeout)
+        .then((Socket socket) {
+      socket.add(ticket.bytes);
+      socket.destroy();
       return Future<PosPrintResult>.value(PosPrintResult.success);
-    } catch (e) {
+    }).catchError((dynamic e) {
       return Future<PosPrintResult>.value(PosPrintResult.timeout);
-    }
+    });
   }
-
-  /// [delayMs]: milliseconds to wait after destroying the socket
-  void disconnect({int? delayMs}) async {
-    _socket.destroy();
-    if (delayMs != null) {
-      await Future.delayed(Duration(milliseconds: delayMs), () => null);
-    }
-  }
-
-  // ************************ Printer Commands ************************
-  void reset() {
-    _socket.add(_generator.reset());
-  }
-
-  void text(
-    String text, {
-    PosStyles styles = const PosStyles(),
-    int linesAfter = 0,
-    bool containsChinese = false,
-    int? maxCharsPerLine,
-  }) {
-    _socket.add(_generator.text(text,
-        styles: styles,
-        linesAfter: linesAfter,
-        containsChinese: containsChinese,
-        maxCharsPerLine: maxCharsPerLine));
-  }
-
-  void setGlobalCodeTable(String codeTable) {
-    _socket.add(_generator.setGlobalCodeTable(codeTable));
-  }
-
-  void setGlobalFont(PosFontType font, {int? maxCharsPerLine}) {
-    _socket
-        .add(_generator.setGlobalFont(font, maxCharsPerLine: maxCharsPerLine));
-  }
-
-  void setStyles(PosStyles styles, {bool isKanji = false}) {
-    _socket.add(_generator.setStyles(styles, isKanji: isKanji));
-  }
-
-  void rawBytes(List<int> cmd, {bool isKanji = false}) {
-    _socket.add(_generator.rawBytes(cmd, isKanji: isKanji));
-  }
-
-  void emptyLines(int n) {
-    _socket.add(_generator.emptyLines(n));
-  }
-
-  void feed(int n) {
-    _socket.add(_generator.feed(n));
-  }
-
-  void cut({PosCutMode mode = PosCutMode.full}) {
-    _socket.add(_generator.cut(mode: mode));
-  }
-
-  void printCodeTable({String? codeTable}) {
-    _socket.add(_generator.printCodeTable(codeTable: codeTable));
-  }
-
-  void beep({int n = 3, PosBeepDuration duration = PosBeepDuration.beep450ms}) {
-    _socket.add(_generator.beep(n: n, duration: duration));
-  }
-
-  void reverseFeed(int n) {
-    _socket.add(_generator.reverseFeed(n));
-  }
-
-  void row(List<PosColumn> cols) {
-    _socket.add(_generator.row(cols));
-  }
-
-  void image(Image imgSrc, {PosAlign align = PosAlign.center}) {
-    _socket.add(_generator.image(imgSrc, align: align));
-  }
-
-  void imageRaster(
-    Image image, {
-    PosAlign align = PosAlign.center,
-    bool highDensityHorizontal = true,
-    bool highDensityVertical = true,
-    PosImageFn imageFn = PosImageFn.bitImageRaster,
-  }) {
-    _socket.add(_generator.imageRaster(
-      image,
-      align: align,
-      highDensityHorizontal: highDensityHorizontal,
-      highDensityVertical: highDensityVertical,
-      imageFn: imageFn,
-    ));
-  }
-
-  void barcode(
-    Barcode barcode, {
-    int? width,
-    int? height,
-    BarcodeFont? font,
-    BarcodeText textPos = BarcodeText.below,
-    PosAlign align = PosAlign.center,
-  }) {
-    _socket.add(_generator.barcode(
-      barcode,
-      width: width,
-      height: height,
-      font: font,
-      textPos: textPos,
-      align: align,
-    ));
-  }
-
-  void qrcode(
-    String text, {
-    PosAlign align = PosAlign.center,
-    QRSize size = QRSize.Size4,
-    QRCorrection cor = QRCorrection.L,
-  }) {
-    _socket.add(_generator.qrcode(text, align: align, size: size, cor: cor));
-  }
-
-  void drawer({PosDrawer pin = PosDrawer.pin2}) {
-    _socket.add(_generator.drawer(pin: pin));
-  }
-
-  void hr({String ch = '-', int? len, int linesAfter = 0}) {
-    _socket.add(_generator.hr(ch: ch, linesAfter: linesAfter));
-  }
-
-  void textEncoded(
-    Uint8List textBytes, {
-    PosStyles styles = const PosStyles(),
-    int linesAfter = 0,
-    int? maxCharsPerLine,
-  }) {
-    _socket.add(_generator.textEncoded(
-      textBytes,
-      styles: styles,
-      linesAfter: linesAfter,
-      maxCharsPerLine: maxCharsPerLine,
-    ));
-  }
-  // ************************ (end) Printer Commands ************************
 }
